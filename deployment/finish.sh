a4eb99f168f8428eb45f264cb66a79cce75b09e4
buffpojken <daniel@sykewarrior.com>
2024-03-17 17:28:36 +0100

    hmm

--- deployment/finish.sh
+++ deployment/finish.sh
@@ -1,1 +1,1 @@
-/home/ec2-user/.nvm/versions/node/v20.10.0/bin/node QUICK_DB_LOCATION=/home/ec2-user/concordia.db /home/ec2-user/concordia/index.js
+QUICK_DB_LOCATION=/home/ec2-user/concordia.db /home/ec2-user/.nvm/versions/node/v20.10.0/bin/node /home/ec2-user/concordia/index.js
