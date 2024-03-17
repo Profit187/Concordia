6826d5ea9256a9391f1d461136eb7f48203a7632
buffpojken <daniel@sykewarrior.com>
2024-03-17 18:05:17 +0100

    more tweaks

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   apps : [{
     name   : "concordia",
-    script : "./index.js",
+    script : "index.js",
+    cwd: "/home/ec2-user/concordia",
     env_production: {
       NODE_ENV: "production", 
       QUICK_DB_LOCATION: "/home/ec2-user/concordia.db"
