1678b0ff481b0b266c2e871d783ebbb008a10418
buffpojken <daniel@sykewarrior.com>
2024-03-17 18:00:28 +0100

    more tweaks

--- deployment/finish.sh
+++ deployment/finish.sh
@@ -1,3 +1,3 @@
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
-pm2 start /home/ec2-user/concordia/pm2.config.js --env production
+pm2 start /home/ec2-user/concordia/pm2.config.cjs --env production
